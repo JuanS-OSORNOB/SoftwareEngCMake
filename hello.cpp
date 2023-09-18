9422a2fb636a94e6fbbda7aa97ea8dd65dcc85c7
Juan Sebastián Osorno Bolívar <juan.osorno@eia.edu.co>
2023-09-18 16:34:38 +0200

    Indicate C++ standard

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <HelloProjConfig.h>
 void hello_world()
 {
     std::cout<<"Hello World!\n";
 }
 
+void print_version()
+{
+    std::cout<<"Project version is "<<HelloProj_VERSION_MAJOR<<"."<<HelloProj_VERSION_MINOR<<std::endl;
+}
 int main()
 {
     hello_world();
+    print_version();
 }
